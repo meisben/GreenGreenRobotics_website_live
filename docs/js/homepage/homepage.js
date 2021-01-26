05139a8680b79b21025e3df8dee22bff26793797
meisben <ben.money+github@gmail.com>
2021-01-26 20:51:49 +0000

    updates

--- docs/js/homepage/homepage.js
+++ docs/js/homepage/homepage.js
@@ -11,19 +11,19 @@
 
 
         // jQuery
-        $.getScript('/js/homepage/svg_bottles_animation.js', function () {
+        $.getScript('./js/homepage/svg_bottles_animation.js', function () {
             console.log("Load was performed for js svg_bottles_animation.");
         });
 
-        $.getScript('/js/homepage/svg_refill_setup.js', function () {
+        $.getScript('./js/homepage/svg_refill_setup.js', function () {
             console.log("Load was performed for js svg_refill_setup.");
         });
 
-        $.getScript('/js/homepage/svg_circular_robotics.js', function () {
+        $.getScript('./js/homepage/svg_circular_robotics.js', function () {
             console.log("Load was performed for js svg_circular_robotics.");
         });
 
-        $.getScript('/js/homepage/svg_team_robot.js', function () {
+        $.getScript('./js/homepage/svg_team_robot.js', function () {
             console.log("Load was performed for js svg_team_robot.");
         });
 
